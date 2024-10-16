540008f3a6ca131981cee0766a6cf48e59cdc466
aldair diaz <irving.s21diaz@gmail.com>
2024-10-16 10:54:38 -0600

    Graphs - Dijkstra's Algorithm

--- graphs/graph_search.py
+++ graphs/graph_search.py
@@ -1,3 +1,7 @@
+from heapq import heappop, heappush
+from math import inf
+
+
 # Depth-first search function:
 def dfs(graph, current_vertex, target_value, visited=None):
     if not visited:
@@ -32,6 +36,28 @@ def bfs(graph, start_vertex, target_value):
                     bfs_queue.append([neighbor, path + [neighbor]])
 
 
+# Dijkstra's search algorithm
+def dijkstra(graph, start):
+    distances = {}
+    for vertex in graph:
+        distances[vertex] = inf
+
+    distances[start] = 0
+    vertices_to_explore = [(0, start)]
+
+    while vertices_to_explore:
+        curren_distance, current_vertex = heappop(vertices_to_explore)
+
+        for neighbor, edge_weight in graph[current_vertex]:
+            new_distance = curren_distance + edge_weight
+
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                heappush(vertices_to_explore, (new_distance, neighbor))
+
+    return distances
+
+
 if __name__ == "__main__":
     some_hazardous_graph = {
         'lava': {'sharks', 'piranhas'},
@@ -41,5 +67,14 @@ if __name__ == "__main__":
         'lasers': set([])
     }
 
+    dijkstra_graph = {
+        'A': [('B', 10), ('C', 3)],
+        'C': [('D', 2)],
+        'D': [('E', 10)],
+        'E': [('A', 7)],
+        'B': [('C', 3), ('D', 2)]
+    }
+
     print(bfs(some_hazardous_graph, 'sharks', 'bees'))
     print(dfs(some_hazardous_graph, 'sharks', 'bees'))
+    print(dijkstra(dijkstra_graph, 'D'))
