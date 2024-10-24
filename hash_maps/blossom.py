ca8dbf27dae968306c83e18653fa4ae7cf6ce460
aldair diaz <irving.s21diaz@gmail.com>
2024-10-24 12:47:37 -0600

    Complete Hash-Maps Project Blossom

--- hash_maps/blossom.py
+++ hash_maps/blossom.py
@@ -1,10 +1,10 @@
-from linear_data_structures.linked_list import Node, LinkedList
+from linear_data_structures.linked_list import LinkedList, Node
 
 
 class HashMap:
     def __init__(self, size):
         self.size: int = size
-        self.array:  list[LinkedList: Node(list)] = [LinkedList()] * self.size
+        self.array = [LinkedList() for _ in range(self.size)]
 
     def hash(self, key) -> int:
         return sum(key.encode())
@@ -15,15 +15,43 @@ class HashMap:
     def assign(self, key, value):
         array_index = self.compress(self.hash(key))
         payload = Node([key, value])
-        list_at_array: LinkedList = self.array[array_index]
+        list_at_array = self.array[array_index]
 
-        for item in list_at_array:
-            if item.get_value()[0] == key:
-                item.set_value(payload)
+        for element in list_at_array:
+            try:
+                if element.get_value()[0] == key:
+                    element.get_value()[1] = value
+            except AttributeError:
+                continue
+
+        list_at_array.insert_beginning(payload)
 
     def retrieve(self, key):
         array_index = self.compress(self.hash(key))
-        payload: LinkedList(list) = self.array[array_index]
 
-        # Todo: fix this implementation
-        return payload.get_head_node().get_value()[1] if payload.get_head_node().get_value()[1] == key else None
+        list_at_index = self.array[array_index]
+        for element in list_at_index:
+            try:
+                if element.get_value()[0] == key:
+                    return element.get_value()[1]
+            except AttributeError:
+                continue
+        return
+
+
+if __name__ == '__main__':
+    # Tests
+    flower_definitions = [['begonia', 'cautiousness'], ['chrysanthemum', 'cheerfulness'], ['carnation', 'memories'],
+                          ['daisy', 'innocence'], ['hyacinth', 'playfulness'], ['lavender', 'devotion'],
+                          ['magnolia', 'dignity'], ['morning glory', 'unrequited love'],
+                          ['periwinkle', 'new friendship'], ['poppy', 'rest'], ['rose', 'love'],
+                          ['snapdragon', 'grace'], ['sunflower', 'longevity'], ['wisteria', 'good luck']]
+
+    blossom = HashMap(len(flower_definitions))
+
+    for flower_def in flower_definitions:
+        blossom.assign(flower_def[0], flower_def[1])
+
+    print(blossom.retrieve('daisy'))
+    blossom.assign('begonia', 'new value')
+    print(blossom.retrieve(key='begonia'))
